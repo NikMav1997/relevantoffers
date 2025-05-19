3f8bc3ca1f689f5f3ba6a324a09a9ba6fad45179
NikMav1997 <razenkovnick@gmail.com>
2025-05-19 17:15:43 +0300

    app.py

--- app.py
+++ app.py
@@ -85,4 +85,6 @@ def logout():
     return redirect(url_for("admin"))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
